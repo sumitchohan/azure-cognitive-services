0911161b459ad1d6db0bbc5b186178586afd3b32
Sumit Chohan <chauhansumit@gmail.com>
2018-10-04 23:13:03 -0500

    test

---
+++ sample.py
@@ -0,0 +1,15 @@
+from azure.cognitiveservices.vision.customvision.training import training_api
+from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateEntry, Region
+
+# Replace with a valid key
+training_key = "91a5eb611da14179a318682958a2a3b5"
+prediction_key = "d1cb98b527624f14a4927d14a19b52be"
+
+trainer = training_api.TrainingApi(training_key)
+
+# Find the object detection domain
+obj_detection_domain = next(domain for domain in trainer.get_domains() if domain.type == "ObjectDetection")
+
+# Create a new project
+print ("Creating project...")
+project = trainer.create_project("My Detection Project", domain_id=obj_detection_domain.id)
